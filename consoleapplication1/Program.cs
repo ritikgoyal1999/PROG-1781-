47b309a368c611ee67687d500346c4669b9a3c41
ritikgoyal1999 <43333676+ritikgoyal1999@users.noreply.github.com>
2018-09-22 19:44:30 +0530

    Add files via upload

---
+++ consoleapplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double m1 = 56.7;
+            double m2 = 65.7;
+            double m3 = 75.7;
+            double average = m1 + m2 + m3 / 3;
+            Console.WriteLine("average of " + m1+","+m2+"and"+m3+"is "+average); 
+       }
+    }
+}
